14194aa4a18dd0e11285521e861a964afb8d7087
HF123 <2212557736@qq.com>
2018-10-16 21:31:22 +0800

    demo

---
+++ demo-admin-server/src/main/java/com/zj/demoadminserver/DemoAdminServerApplication.java
@@ -0,0 +1,17 @@
+package com.zj.demoadminserver;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+public class DemoAdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAdminServerApplication.class, args);
+	}
+}
